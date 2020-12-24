224e36e3409202a153c0137599a87f1c1bcc6628
Mosharraf Hossain <mail2mhossain@gmail.com>
2020-12-24 20:14:55 +0600

    base model selection

---
+++ stroke_detection/src/scripts/preparation/Base_Model_Selection.py
@@ -0,0 +1,446 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,../scripts/preparation//py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: PyCaret_Env
+#     language: python
+#     name: py_caret_env
+# ---
+
+# + [markdown] heading_collapsed=true
+# ## Import Libraries
+
+# + hidden=true
+from datetime import datetime
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import PowerTransformer, QuantileTransformer
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold, StratifiedKFold, RepeatedStratifiedKFold
+from tpot import TPOTClassifier
+from hpsklearn import HyperoptEstimator, min_max_scaler, standard_scaler, one_hot_encoder, pca
+from hpsklearn import any_classifier
+from hpsklearn import any_preprocessing
+from hyperopt import tpe, hp
+from pycaret.classification import * 
+from pycaret.datasets import get_data
+import mlflow
+import mlflow.sklearn
+from imblearn.over_sampling import ADASYN  #Its a improved version of Smote
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from imblearn.ensemble import BalancedRandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import BaggingClassifier
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import BorderlineSMOTE
+from imblearn.over_sampling import SVMSMOTE
+from imblearn.over_sampling import ADASYN
+from imblearn.pipeline import Pipeline
+from sklearn.metrics import make_scorer
+from imblearn.metrics import geometric_mean_score
+from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix
+
+# + [markdown] heading_collapsed=true
+# ## Load Clean Data
+
+# + hidden=true
+#"../../data/processed/stroke_data_processed_important_3_features.csv"
+#"../../data/processed/stroke_data_processed_important_2_features.csv"
+df = pd.read_csv("../../data/processed/stroke_data_processed_important_2_features.csv")
+
+# + hidden=true
+df.shape
+
+# + hidden=true
+X = df.drop(['stroke'],axis=1)
+y = df['stroke']
+
+# + hidden=true
+y = LabelEncoder().fit_transform(y)
+
+# + [markdown] heading_collapsed=true
+# ## Train Test Split
+
+# + hidden=true
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, stratify=y, random_state=42)
+
+# + hidden=true
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.25, stratify=y_train, random_state=42)
+
+# + hidden=true
+print (f"Train Data (X): {X_train.shape}")
+print (f"Test Data (X): {X_test.shape}")
+print (f"Validation Data (X): {X_valid.shape}")
+
+# + hidden=true
+print (f"Train Data (y): {y_train.shape}")
+print (f"Test Data (y): {y_test.shape}")
+print (f"Validation Data (y): {y_valid.shape}")
+
+
+# + [markdown] heading_collapsed=true
+# ## Shape of the Dataset
+
+# + hidden=true
+def get_weight():
+    from collections import Counter
+    # summarize the shape of the dataset
+    print(X.shape)
+    counter = Counter(y)
+    for k,v in counter.items():
+        per = v / len(y) * 100
+        print('Class=%s, Count=%d, Percentage=%.3f%%' % (k, v, per))
+    weight = counter[0] / counter[1]
+    return (int(weight))
+
+
+# + hidden=true
+weights = get_weight()
+print(weights)
+
+
+# + [markdown] heading_collapsed=true
+# ## Evaluation Method
+
+# + hidden=true
+# evaluate a model
+def evaluate_model(X, y, model):
+    # define evaluation procedure
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+    # define the model evaluation the metric
+    metric = make_scorer(geometric_mean_score)
+    # evaluate model
+    scores = cross_val_score(model, X, y, scoring=metric, cv=cv, n_jobs=-1)
+    return scores
+
+
+# + [markdown] heading_collapsed=true
+# ## Find Best Algorithm using AutoML
+
+# + [markdown] heading_collapsed=true hidden=true
+# ### Find Best Algorithm with Best Params Using PyCaret
+
+# + hidden=true
+clf = setup(data = df, target = 'stroke', session_id=123,
+        train_size = 0.7,
+        data_split_stratify = True,
+        #sampling = False,
+        #categorical_features = cat_features,
+        normalize = True, 
+        normalize_method = 'minmax',
+        #transformation = True, 
+        #transformation_method = 'quantile',
+        #pca = True,
+        #pca_method = 'incremental',
+        #feature_selection = True,
+        #feature_selection_threshold = 0.7,
+        #feature_selection_method = 'boruta',
+        #remove_outliers = True,
+        #outliers_threshold = 0.05,
+        fix_imbalance = True,
+        fix_imbalance_method = ADASYN() 
+        #profile = True
+       )
+
+# + hidden=true
+#with mlflow.start_run():
+best = compare_models()
+best_model = finalize_model(best)
+tuned_model = tune_model(best_model, optimize = 'F1') #Accuracy, AUC, Recall, Precision, F1, Kappa, MCC
+predictions = predict_model(tuned_model)
+
+#mlflow.log_metric('accuracy', predictions)
+#mlflow.sklearn.log_model(tuned_model, 'model')
+
+# + hidden=true
+best_model = finalize_model(best)
+
+# + hidden=true
+plot_model(best_model, plot = 'confusion_matrix')
+
+# + hidden=true
+plot_model(best_model, plot='feature')
+
+# + hidden=true
+# define the data preparation and modeling pipeline
+pipeline = Pipeline(steps=[('prep', col_transform), ('m', best_model)])
+
+# + hidden=true
+pipeline.fit(X_train, y_train)
+
+# + hidden=true
+print(classification_report(y_test, pipeline.predict(X_test)))
+
+# + hidden=true
+plot_confusion_matrix(pipeline, X_test, y_test, values_format='d', display_labels=["Stroke","Not Stroke"]);
+
+# + [markdown] heading_collapsed=true hidden=true
+# ### Find Best Algorithm with Best Params Using HyperoptEstimator AutoML
+
+# + hidden=true
+preproc = hp.choice('myprepros_name', 
+                    [
+                        [min_max_scaler('myprepros_name.norm')],
+                        [standard_scaler('myprepros_name.std_scaler')],
+                        [min_max_scaler('myprepros_name.norm2'), standard_scaler('myprepros_name.std_scaler2')]
+                    ])
+
+# + hidden=true
+#with mlflow.start_run():
+model = HyperoptEstimator(  classifier= any_classifier('cla'), 
+                            preprocessing= preproc, #any_preprocessing('pre'), 
+                            algo=tpe.suggest, 
+                            max_evals=50, 
+                            trial_timeout=5000)
+# perform the search
+model.fit(X_train, y_train)
+accuracy = model.score(X_test, y_test)
+
+#mlflow.log_params(params)
+#mlflow.log_metric('accuracy', accuracy)
+
+# Logging training data
+#mlflow.log_artifact(local_path = '../Data/higgs_boson_training.csv')
+
+# Logging training code
+#mlflow.log_artifact(local_path = './Base_Model_Selection.ipynb')
+
+# Logging model to MLFlow
+#mlflow.sklearn.log_model(model.best_model(), 'model')
+#summarize the best model
+print("Accuracy: %.3f" % accuracy)
+print(model.best_model())
+# + hidden=true
+
+
+
+# + [markdown] heading_collapsed=true hidden=true
+# ### Find Best Algorithm with Best Params Using TPOT
+
+# + hidden=true
+#with mlflow.start_run():
+# define the model evaluation the metric
+metric = make_scorer(geometric_mean_score)
+# define model evaluation
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+# define search
+#model = TPOTClassifier(generations=5, population_size=50, cv=cv, scoring='accuracy', 
+                       #verbosity=2, random_state=1, n_jobs=-1)
+tpot = TPOTClassifier(max_time_mins=180, cv=cv, scoring=metric, 
+                       verbosity=2, random_state=1, n_jobs=-1)
+# perform the search
+tpot.fit(X_train, y_train)
+
+predictions = tpot.score(X_test, y_test)
+
+model = tpot.fitted_pipeline_
+
+#mlflow.log_metric('accuracy', predictions)
+#mlflow.sklearn.log_model(model, 'model')
+
+# + hidden=true
+print(predictions)
+
+# + hidden=true
+print(model)
+
+
+# + [markdown] heading_collapsed=true
+# ## Spot Check Balanced Model Algorithms
+
+# + hidden=true
+# define models to test
+def get_balanced_models():
+    models = list()   
+    #LR
+    models.append(('LR_Bal', LogisticRegression(solver='lbfgs', class_weight='balanced'))) 
+    # LDA
+    models.append(('LDA', LinearDiscriminantAnalysis()))
+    #KNN
+    models.append(('KNN', KNeighborsClassifier()))
+    #NB
+    models.append(('NB', GaussianNB()))
+    #MNB
+    #models.append(('MNB', MultinomialNB()))
+    #GPC
+    #models.append(('GPC', GaussianProcessClassifier()))
+    if X.shape[0] < 100000:
+        #SVM Balanced
+        models.append(('SVM_Bal', SVC(gamma='scale', class_weight='balanced')))    
+        #SVM Weight
+        models.append(('SVM_W', SVC(gamma='scale', class_weight=weights)))    
+    #Balanced RF
+    models.append(('Bal_RF', BalancedRandomForestClassifier(n_estimators=1000)))      
+    #RF
+    models.append(('RF_Bal', RandomForestClassifier(n_estimators=1000, class_weight='balanced')))
+    #DT
+    models.append(('DT_Bal', DecisionTreeClassifier(class_weight='balanced')))
+    #Bag
+    models.append(('BAG', BaggingClassifier(n_estimators=1000)))
+    #XGB
+    models.append(('XGB_W', XGBClassifier(scale_pos_weight=weights))) 
+    return models
+# + hidden=true
+init_time = datetime.now()
+models = get_balanced_models()
+for name, model in models:
+    #pipeline = Pipeline(steps=[('prep', col_transform), ('m', model)])
+    pipeline = Pipeline(steps=[('norm', MinMaxScaler()), ('m', model)])
+    scores = evaluate_model(X, y, pipeline)
+    print(f"Geometric mean score of {name}: {np.mean(scores):.3f} ({np.std(scores):.3f})")
+fin_time = datetime.now()
+print("Execution time : ", (fin_time-init_time))
+
+
+# + [markdown] heading_collapsed=true
+# ## Spot Check Ensemble Algorithms with Over Sampling
+
+# + hidden=true
+# define models to test
+def get_ensemble_models():
+    models = list()
+    #LR
+    models.append(('LR', LogisticRegression(solver='lbfgs',class_weight='balanced'))) 
+    # LDA
+    models.append(('LDA', LinearDiscriminantAnalysis()))
+    #KNN
+    models.append(('KNN', KNeighborsClassifier()))
+    #NB
+    models.append(('NB', GaussianNB()))
+    #MNB
+    #models.append(('MNB', MultinomialNB()))
+    #GPC
+    #models.append(('GPC', GaussianProcessClassifier()))
+    #Balanced RF
+    models.append(('Bal_RF', BalancedRandomForestClassifier(n_estimators=1000)))
+    #DT
+    models.append(('DT', DecisionTreeClassifier()))
+    #Bag
+    models.append(('BAG', BaggingClassifier()))
+    #SGD
+    models.append(('SGD', SGDClassifier()))
+    #ADA
+    models.append(('AD', AdaBoostClassifier())) 
+    #ET 
+    models.append(('ET', ExtraTreesClassifier()))
+    #RF
+    models.append(('RF', RandomForestClassifier(n_estimators=1000)))
+    #GBM
+    models.append(('GBM', GradientBoostingClassifier(n_estimators=1000)))
+    #XGB
+    models.append(('XGB', XGBClassifier()))
+    #LGB
+    models.append(('LGB', LGBMClassifier()))
+    #CAT
+    models.append(('CAT', CatBoostClassifier()))
+    return models
+
+
+# + hidden=true
+init_time = datetime.now()
+models = get_ensemble_models() 
+for name, model in models:
+    #pipeline = Pipeline(steps=[('prep', col_transform), ('over', RandomOverSampler()), ('m', model)])
+    pipeline = Pipeline(steps=[('norm', MinMaxScaler()), ('over', RandomOverSampler()),  ('m', model)])
+    scores = evaluate_model(X, y, pipeline)
+    print(f"Geometric mean score of {name}: {np.mean(scores):.3f} ({np.std(scores):.3f})")
+fin_time = datetime.now()
+print("Execution time : ", (fin_time-init_time))
+
+
+# + [markdown] heading_collapsed=true
+# ## Over Sampling
+
+# + hidden=true
+# define oversampling models to test
+def get_oversampling_models():
+    models, names = list(), list()
+    # RandomOverSampler
+    models.append(RandomOverSampler())
+    names.append('ROS')
+    # SMOTE
+    models.append(SMOTE())
+    names.append('SMOTE')
+    # BorderlineSMOTE
+    models.append(BorderlineSMOTE())
+    names.append('BLSMOTE')
+    # SVMSMOTE
+    models.append(SVMSMOTE())
+    names.append('SVMSMOTE')
+    # ADASYN
+    models.append(ADASYN())
+    names.append('ADASYN')
+    return models, names
+
+
+# + hidden=true
+# define models
+oversamples, names = get_oversampling_models()
+# evaluate each model
+for i in range(len(oversamples)):
+    # define the model
+    model = LogisticRegression(solver='lbfgs',class_weight='balanced')
+    # define the pipeline steps
+    steps = [('prep', col_transform), ('over', oversamples[i]), ('m', model)]
+    # define the pipeline
+    pipeline = Pipeline(steps=steps)
+    # evaluate the model and store results
+    scores = evaluate_model(X, y, pipeline)
+    # summarize and store
+    print(f"Geometric mean score of {names[i]}: {np.mean(scores):.3f} ({np.std(scores):.3f})")
+
+
+# + [markdown] heading_collapsed=true
+# ## Best Model
+
+
+# + hidden=true
+# define the model
+model = LogisticRegression(solver='lbfgs', class_weight='balanced')
+# define the pipeline steps
+steps = [('prep', col_transform), ('norm', MinMaxScaler()), ('power', PowerTransformer()), \
+         ('over', RandomOverSampler()), ('m', model)]
+# define the pipeline
+pipeline = Pipeline(steps=steps)
+pipeline.fit(X_train, y_train)
+prediction = pipeline.predict(X_test)
+
+# + hidden=true
+print (classification_report(y_test, prediction))
+
+# + hidden=true
+plot_confusion_matrix(pipeline, X_test, y_test)
+
+# + [markdown] heading_collapsed=true
+# ## Parameter Optimization
+
+# + hidden=true
+
+
+# + hidden=true
+
--- stroke_detection/src/scripts/preparation/data_preparation.py
+++ stroke_detection/src/scripts/preparation/data_preparation.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -8,15 +9,13 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.7.1
 #   kernelspec:
-#     display_name: Stroke_Detection
+#     display_name: PyCaret_Env
 #     language: python
-#     name: stroke_detection
+#     name: py_caret_env
 # ---
 
-# + [markdown] heading_collapsed=true
 # ## Import Libraries
 
-# + hidden=true
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -24,15 +23,23 @@ import datasist.project as dp
 import datasist as ds
 from pandas_profiling import ProfileReport
 import sweetviz as sv
+from boruta import BorutaPy
+from BorutaShap import BorutaShap
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.preprocessing import PowerTransformer, QuantileTransformer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold, StratifiedKFold, RepeatedStratifiedKFold
+from sklearn.metrics import classification_report,confusion_matrix,plot_confusion_matrix
+from sklearn.metrics import make_scorer
+from imblearn.metrics import geometric_mean_score
 
 # + [markdown] heading_collapsed=true
 # ## Load Data
@@ -63,9 +70,6 @@ profile = ProfileReport(df, title='Pandas Profiling Report', explorative=True)
 # + hidden=true
 profile.to_file("../../outputs/profiling/Stroke_Detection_pandas_profiling.html")
 
-# + hidden=true
-df.columns
-
 # + [markdown] heading_collapsed=true hidden=true
 # ### sweetviz profiling
 
@@ -80,6 +84,9 @@ my_report.show_html('../../outputs/profiling/Stroke_Detection_pandas_sv_profilin
 # ### Summary Of EDA
 
 # + hidden=true
+df.columns
+
+# + hidden=true
 categorical_features = ['gender', 'hypertension', 'heart_disease', 'ever_married', 'work_type', \
                         'Residence_type', 'smoking_status']
 
@@ -90,81 +97,237 @@ normally_distributed_features = ['age','bmi']
 skewed_distributed_features = ['avg_glucose_level']
 
 # + [markdown] heading_collapsed=true
+# ## Normality Test
+#
+# * NN Rule: Normalize Non-Gaussian
+
+# + [markdown] hidden=true
+# * Standardize Only Gaussian-Like Input Variables
+# * Normalize Only Non-Gaussian Input Variables
+
+# + hidden=true
+from scipy.stats import shapiro, normaltest
+
+
+# + hidden=true
+def is_normal(data):
+    alpha = 0.05
+    stat, p = normaltest(data)
+    if p > alpha:
+        normalTest = True
+    else:
+        normalTest = False
+    
+    stat, p = shapiro(data)
+    if p > alpha:
+        shapiroTest = True
+    else:
+        shapiroTest = False
+        
+    return normalTest and shapiroTest
+
+
+# + hidden=true
+numeic_features = ['age','bmi', 'avg_glucose_level']
+
+# + hidden=true
+Gaussian_Like = []
+Non_Gaussian = []
+
+for i, name in enumerate (numeic_features):
+    if is_normal(X[name]):
+        Gaussian_Like.append(name)
+    else:
+        Non_Gaussian.append(name)
+        
+print (f"Gaussian Like columns: {Gaussian_Like}")
+print (f"Non-Gaussian Like columns: {Non_Gaussian}")
+# -
+
 # ## Data Preprocessing
 
-# + [markdown] heading_collapsed=true hidden=true
+# + [markdown] heading_collapsed=true
 # ### Remove Duplicate Data
 
 # + [markdown] hidden=true
 # There are no duplicate rows
 
-# + [markdown] heading_collapsed=true hidden=true
+# + [markdown] heading_collapsed=true
 # ### Identify and Remove column variables that only have a single value.
 
 # + [markdown] hidden=true
 # There is no such column
 
-# + [markdown] heading_collapsed=true hidden=true
+# + [markdown] heading_collapsed=true
 # ### Missing Data Imputation
 
 # + [markdown] hidden=true
 # There is missing values
 
-# + [markdown] heading_collapsed=true hidden=true
+# + [markdown] heading_collapsed=true
 # ### Handling Categorical Columns
 
 # + hidden=true
+df_one_hot= pd.get_dummies(df, columns=categorical_features, drop_first=True)
 
-
-
-# + [markdown] heading_collapsed=true
-# ## Save Clean Data
 
 # + hidden=true
-dp.save_data(data=df, name='processed_data', method='csv', loc='processed')
+df_one_hot.columns
 
 # + [markdown] heading_collapsed=true
-# ## Train Test Split
+# ### Handling Target Value
 
 # + hidden=true
-X = df.drop(['stroke'],axis=1)
-y = df['stroke']
+X_one_hot = df_one_hot.drop(['stroke'],axis=1)
+y = df_one_hot['stroke']
 
 # + hidden=true
 y = LabelEncoder().fit_transform(y)
+# -
 
-# + hidden=true
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, stratify=y, random_state=42)
+# ## Feature Selection
+
+X_one_hot.columns
+
+X_one_hot.shape
 
 # + [markdown] heading_collapsed=true
-# ## Pipeline Preparation and Evaluation
+# ### Boruta
 
 # + hidden=true
+forest = RandomForestClassifier(n_jobs = -1, max_depth = 5) 
+boruta = BorutaPy(
+   estimator = forest, 
+   n_estimators = 'auto',
+   max_iter = 100 #number of trials to perform
+)
+
+# + hidden=true
+#fit Boruta (it accepts np.array, not pd.DataFrame)
+boruta.fit(np.array(X_one_hot), np.array(y))
+
+# + hidden=true
+X_filtered = boruta.transform(np.array(X_one_hot))
+X_filtered.shape
+
+# + hidden=true
+#print results
+green_area = X_one_hot.columns[boruta.support_].to_list()
+blue_area = X_one_hot.columns[boruta.support_weak_].to_list()
+print('features in the green area:', green_area)
+print('features in the blue area:', blue_area)
+
+# + hidden=true
+X_filtered = pd.DataFrame(X_filtered)
+X_filtered.columns = ['age', 'avg_glucose_level', 'heart_disease_1']
+
+# + hidden=true
+print (X_filtered.shape)
+X_filtered.head()
+
+# + hidden=true
+X_new = X_one_hot[['age', 'avg_glucose_level', 'heart_disease_1']]
+X_new.head()
+# -
+
+# ### BorutaShap
+
+# This initialization takes a maximum of 5 parameters including a tree based model of your choice example a **“Decision Tree” or “XGBoost” or "CatBoost" default is a “Random Forest”**. Which importance metric you would like to evaluate the features importance with either **Shapley values (default) or Gini importance**, A flag to specify if the problem is either classification or regression, a percentile parameter which will take a percentage of the max shadow feature thus making the selector less strict and finally a p-value or significance level which a after a feature will be either rejected or accepted.
+
+model_xgb = XGBClassifier(objective='binary:logistic')
+Feature_Selector_xgb = BorutaShap(model=model_xgb,
+                              importance_measure='shap',
+                              classification=True)
+Feature_Selector_xgb.fit(X=X_one_hot, y=y, n_trials=25, random_state=0)
+
+#Returns a subset of the original data with the selected features
+X_subset_xgb = Feature_Selector_xgb.Subset()
+print (X_subset_xgb.shape)
+X_subset_xgb.head()
+
+model_cat = CatBoostClassifier()
+Feature_Selector_cat = BorutaShap(model=model_cat,
+                              importance_measure='shap',
+                              classification=True)
+Feature_Selector_cat.fit(X=X_one_hot, y=y, n_trials=25, random_state=0)
+
+#Returns a subset of the original data with the selected features
+X_subset_cat = Feature_Selector_cat.Subset()
+print (X_subset_cat.shape)
+X_subset_cat.head()
+
+X_cat = X_one_hot[['age', 'ever_married_Yes', 'avg_glucose_level', 'bmi']]
+
+# ### Custom Data based on Boruta Results
+
+X_new = X_one_hot[['age', 'avg_glucose_level', 'heart_disease_1', 'bmi', 'ever_married_Yes']]
+
+# ## Train Test Split
+
+X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size=0.30, stratify=y, random_state=42)
+
+
+# + [markdown] heading_collapsed=true
+# ## Class Ratio Calculation
+
+# + hidden=true
+def get_weight():
+    from collections import Counter
+    # summarize the shape of the dataset
+    print(X_subset_xgb.shape)
+    counter = Counter(y)
+    for k,v in counter.items():
+        per = v / len(y) * 100
+        print('Class=%s, Count=%d, Percentage=%.3f%%' % (k, v, per))
+    weight = counter[0] / counter[1]
+    return (int(weight))
+
+
+# + hidden=true
+weights = get_weight()
+print(weights)
+# -
+
+# ## Data Evaluation
+
 # define the data preparation for the columns
-t = [('cat', OneHotEncoder(drop='first', sparse=False), categorical_features), \
-     ('min_max', MinMaxScaler(), skewed_distributed_features),\
-     ('scaler', StandardScaler(), normally_distributed_features),\
-    ('power', PowerTransformer(), normally_distributed_features)]
-col_transform = ColumnTransformer(transformers=t)
+# t = [('cat', OneHotEncoder(drop='first', sparse=False), categorical_features), \
+#      ('min_max', MinMaxScaler(), skewed_distributed_features),\
+#      ('scaler', StandardScaler(), normally_distributed_features)]
+#     #('power', PowerTransformer(), normally_distributed_features)]
+# col_transform = ColumnTransformer(transformers=t)
 
-# + hidden=true
-# define the model
-model = RandomForestClassifier(n_estimators=1000, class_weight='balanced')
+#define the model evaluation the metric
+metric = make_scorer(geometric_mean_score)
 
-# + hidden=true
-# define the data preparation and modeling pipeline
-pipeline = Pipeline(steps=[('prep',col_transform), ('m', model)])
-
-# + hidden=true
-# define the model cross-validation configuration
+#define the model cross-validation configuration
 cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 
-# + hidden=true
-# evaluate the pipeline using cross validation and calculate MAE
-scores = cross_val_score(pipeline, X, y, scoring='f1_micro', cv=cv, n_jobs=-1)
+model = LogisticRegression(solver='lbfgs', class_weight='balanced')
+pipeline = Pipeline(steps=[('norm', MinMaxScaler()), ('m', model)])
+scores = cross_val_score(pipeline, X_new, y, scoring=metric, cv=cv, n_jobs=-1)
+print('Geometric mean score: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
 
-# + hidden=true
-print('F Score: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
+pipeline.fit(X_train, y_train)
 
-# + hidden=true
+print(classification_report(y_test, pipeline.predict(X_test)))
+
+plot_confusion_matrix(pipeline, X_test, y_test, values_format='d', display_labels=["Stroke","Not Stroke"]);
+
+# ## Save Clean Data
+
+df_one_hot.shape
+
+df_one_hot['stroke'].head()
+
+df_one_hot['stroke'] = y
+
+df_one_hot['stroke'].head()
+
+df_one_hot.to_csv("../../data/processed/stroke_data_processed_all_features.csv") 
+
+df_boruta = df_one_hot[['age', 'avg_glucose_level', 'heart_disease_1', 'bmi', 'ever_married_Yes', 'stroke']]
+
+df_boruta.to_csv("../../data/processed/stroke_data_processed_important_features.csv") 
+
 
